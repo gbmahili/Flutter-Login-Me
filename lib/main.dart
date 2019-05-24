8d284eba1c5107d273ff5bab6ef7264be3d8b0ab
Baraka Mahili <georgesmahili@gmail.com>
2019-05-24 10:26:06 -0400

    Added image and borders

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,10 @@ class App extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  final String baraka =
+  final String image =
       'https://lh3.googleusercontent.com/Dsmyq3u-WH9bk_BkEaRhBzV_m4B9i0yduSmeq-e35WTWgM44h3Zff_MckpMlDKM3WL9lY9c0CWTZPuyViGVsoHchZZ0PhHKuYqg_uG-K4dJ8I1k_2YL1w629YHpv1JlYEygXEtiD34demLYcv7FTtPHxYnubXG3Tr9A6fN-K0Mr6yAox7T99XW7uM1ken7ZC5hmuUPemY5Oxe8V2D9VHW4c3JgvKzWtXU64DlRWfJbO-o6kTj1XYmTv3ry1kRGJW3tCJffBy36l2JO8H0C573y7h8nSbz94AjPMZ2r_pMY2aMojU9f-qxcZu5jaY0EPJSZHGeAXOA1zRR1IqB63yHeLGb2Jbaimhmq_Wmq9hliXD4nIhvJhPp2KcJ-9CRXIsPfM26JcfVrxHtro3yRrIFKaszi8NE1jW3K2nZqPBtLAa9PzXXoMRvOHd0ti8Om3gXKP7dImBTv3-r5jvliVooQtc29ev6DQbsWHm53FwRVZ_B5eusMgvmiiZY4MmSppY7aPdXq4Nze2_bm60ue-bbRAPSlt7f2yIhgZ3PB6tt3YpjYgRrWtjNbifOEpH3KdMNzZV9I2Vinb9cdH01MXo6Jmk-iXk7eLYLVRbsx1u_zItf2VOxoXqOksrkm5Ek5YFWP6mvXNGoKZcwf3yvbjr3wM2-CkAaMi0=w473-h969-no';
-
+  String image1 =
+      'https://images.pexels.com/photos/2300641/pexels-photo-2300641.jpeg?cs=srgb&dl=attractive-beautiful-beauty-2300641.jpg&fm=jpg';
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -26,7 +27,12 @@ class HomePage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Positioned(
-            child: Image.network(baraka),
+            // child: Image.network(
+            //   image1,
+            //   alignment: Alignment.center,
+            // ),
+            child: BarakaImage(),
+            height: screenHeight,
             bottom: 0,
             left: -30,
           ),
@@ -55,16 +61,35 @@ class HomePage extends StatelessWidget {
                           fontSize: 25,
                           fontWeight: FontWeight.w100),
                     ),
-                    Text('hi'),
-                    Text('hi'),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Material(
-                        borderRadius: BorderRadius.circular(30.0),
-                        
+                    Form(
+                      child: Column(
+                        children: <Widget>[
+                          new FormItem('Email'),
+                          new FormItem('Password'),
+                        ],
                       ),
                     ),
-                    Text('hi'),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 1.0),
+                      child: Material(
+                        borderRadius: BorderRadius.circular(30.0),
+                        shadowColor: Colors.white,
+                        elevation: 3.0,
+                        color: Color.fromRGBO(0, 0, 20, 1.0),
+                        child: MaterialButton(
+                          minWidth: 200.0,
+                          child: Text(
+                            'SIGN IN',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () async {},
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ],
                 ),
               ),
@@ -73,5 +98,42 @@ class HomePage extends StatelessWidget {
         ],
       ),
     ));
+  }
+}
+
+class BarakaImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/baraka.jpg');
+    //Image image = Image(image: assetImage);
+    return Image(image: assetImage);
+  }
+}
+
+class FormItem extends StatelessWidget {
+  final String hintText;
+  FormItem(this.hintText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.white,
+        ),
+        child: TextFormField(
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            hintText: hintText,
+            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(15.0)),
+          ),
+        ),
+      ),
+    );
   }
 }
